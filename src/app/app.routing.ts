5570c9edd46f897a3bc70510aa0b8cb35ab7d61f
Vu_T-Solution <leanhvu86@gmail.com>
2020-06-08 01:21:03 +0700

    update admin page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,44 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import {DashboardLayoutComponent} from './layouts/dashboard-layout/dashboard-layout.component';
-
-const routes: Routes = [
+import { Routes } from '@angular/router';
+export const AppRoutes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full',
   },
-  /*{
-    path: '',
-    component: DashboardLayoutComponent,
-    children: [{
-      path: '',
-      loadChildren: './layouts/dashboard-layout/dashboard-layout.module#DashboardLayoutModule'
-    }]
-  }*/
   {
     path: '',
-    component: AdminLayoutComponent,
-    children: [{
-      path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-    }]
+    loadChildren: './layouts/dashboard-layout/dashboard-layout.module#DashboardLayoutModule'
+  },
+  {
+    path: '',
+    loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
   }
 ];
-
-@NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule.forRoot(routes,{
-       useHash: true
-    })
-  ],
-  exports: [
-  ],
-})
-export class AppRoutingModule { }
